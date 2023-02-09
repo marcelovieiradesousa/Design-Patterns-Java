d8f1d9fee8619ca64232b8cb72ab4c2f9a695313
Marcelo Vieira <marcelovieira.tec@gmail.com>
2023-02-09 16:00:00 -0300

    fix

--- Facade/Estrutural/Facade/src/Facade.java
+++
@@ -1,8 +0,0 @@
-public class Facade {
-    public void migrarCliente(String nome, String cep) {
-        String cidade = Cep.getInstance().recuperarCidade(cep);
-        String estado = Cep.getInstance().recuperarEstado(cep);
-
-        CrmService.gravarCliente(nome, cep, cidade, estado);
-    }
-}
